963ae8c5b65e50e220e0ca41d1461e53a7c3b568
Aaron <escwod@gmail.com>
2020-05-18 23:13:11 +0800

    SpringBoot基础相关代码初次提交

---
+++ springboot-basic1/src/main/java/com/aaron/SpringbootBasic1Application.java
@@ -0,0 +1,13 @@
+package com.aaron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasic1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBasic1Application.class, args);
+    }
+
+}
